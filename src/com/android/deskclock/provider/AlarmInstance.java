9af7c3842e0cefe3bb4e285750f6412a9e2d895d
Michael Bestas <mkbestas@gmail.com>
2026-05-19 02:58:45 +0300

    Revert "Revert LineageOS db migration changes"
    This reverts commit 2810de34bd6f9828e5583d6404e8f9e5fbd6236f.
    
    Reason for revert: No reason to break database compatibility.
    This should fix backup/restore between different ROMs.
    
    Change-Id: Id880c037e913c8248668d6d5108a449775be9614

--- src/com/android/deskclock/provider/AlarmInstance.java
+++ src/com/android/deskclock/provider/AlarmInstance.java
@@ -68,7 +68,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             VIBRATE,
             RINGTONE,
             ALARM_ID,
-            ALARM_STATE
+            ALARM_STATE,
+            INCREASING_VOLUME
     };
 
     /**
@@ -86,8 +87,9 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     private static final int RINGTONE_INDEX = 8;
     private static final int ALARM_ID_INDEX = 9;
     private static final int ALARM_STATE_INDEX = 10;
+    private static final int INCREASING_VOLUME_INDEX = 11;
 
-    private static final int COLUMN_COUNT = ALARM_STATE_INDEX + 1;
+    private static final int COLUMN_COUNT = INCREASING_VOLUME_INDEX + 1;
 
     public static ContentValues createContentValues(AlarmInstance instance) {
         ContentValues values = new ContentValues(COLUMN_COUNT);
@@ -111,6 +113,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         }
         values.put(ALARM_ID, instance.mAlarmId);
         values.put(ALARM_STATE, instance.mAlarmState);
+        values.put(INCREASING_VOLUME, instance.mIncreasingVolume ? 1 : 0);
+
         return values;
     }
 
@@ -270,6 +274,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     public Uri mRingtone;
     public Long mAlarmId;
     public int mAlarmState;
+    public boolean mIncreasingVolume;
 
     public AlarmInstance(Calendar calendar, Long alarmId) {
         this(calendar);
@@ -283,6 +288,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         mVibrate = false;
         mRingtone = null;
         mAlarmState = SILENT_STATE;
+        mIncreasingVolume = false;
     }
 
     public AlarmInstance(AlarmInstance instance) {
@@ -297,6 +303,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
          this.mRingtone = instance.mRingtone;
          this.mAlarmId = instance.mAlarmId;
          this.mAlarmState = instance.mAlarmState;
+         this.mIncreasingVolume = instance.mIncreasingVolume;
     }
 
     public AlarmInstance(Cursor c, boolean joinedTable) {
@@ -331,6 +338,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             mAlarmId = c.getLong(ALARM_ID_INDEX);
         }
         mAlarmState = c.getInt(ALARM_STATE_INDEX);
+        mIncreasingVolume = c.getInt(INCREASING_VOLUME_INDEX) == 1;
     }
 
     public String getLabelOrDefault(Context context) {
@@ -440,6 +448,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
                 ", mRingtone=" + mRingtone +
                 ", mAlarmId=" + mAlarmId +
                 ", mAlarmState=" + mAlarmState +
+                ", mIncreasingVolume=" + mIncreasingVolume +
                 '}';
     }
 }
